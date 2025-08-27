0557076ec1ba21097f98fb91e35913bf53ec7e59
Matheus Librelon <matheuslibrelon770@gmail.com>
2025-08-26 21:24:28 -0300

    Add files via upload

---
+++ Exercicio.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Exercicio {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        double temperaturaum;
+        double temperaturadois;
+        double temperaturatres;
+
+        System.out.println("Qual foi a temperatura do primeiro dia em Cº? ");
+        temperaturaum = entrada.nextDouble();
+        System.out.println("Qual foi a temperatura do segundo dis em Cº? ");
+        temperaturadois = entrada.nextDouble();
+        System.out.println("Qual foi a temperatura do terceiro dis em Cº? ");
+        temperaturatres = entrada.nextDouble();
+
+        double media = (temperaturaum + temperaturadois +temperaturatres) / 3;
+
+        System.out.printf("A média das três temperaturas será: %sCº\n", media);
+
+    }
+}
